05c3ee533e1c6f400ed1424601b8bad6aac8096f
Rolka111111 <90156401+Rolka111111@users.noreply.github.com>
2023-03-30 22:51:45 +0300

    Update wg.sh

--- wireguard/wg.sh
+++ wireguard/wg.sh
@@ -18,7 +18,7 @@ LIGHT='\033[0;37m'
 # Getting
 MYIP=$(wget -qO- ipinfo.io/ip);
 echo "Checking VPS"
-IZIN=$( curl https://raw.githubusercontent.com/Slehibot/SLv2ray/main/ipvps.txt | grep $MYIP )
+IZIN=$( curl https://raw.githubusercontent.com/Rolka111111/SLv2ray/main/ipvps.txt | grep $MYIP )
 if [ $MYIP = $MYIP ]; then
 echo -e "${NC}${GREEN}Permission Accepted...${NC}"
 else
@@ -27,7 +27,7 @@ exit 0
 fi
 # ==================================================
 # Link Hosting Kalian
-lakmalvpn="raw.githubusercontent.com/Slehibot/SLv2ray/main/wireguard"
+lakmalvpn="raw.githubusercontent.com/Rolka111111/SLv2ray/main/wireguard"
 
 # Check OS version
 if [[ -e /etc/debian_version ]]; then
