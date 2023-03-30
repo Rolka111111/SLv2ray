69da158b88ae0b20435991ccfc7440dfef8becd4
Rolka111111 <90156401+Rolka111111@users.noreply.github.com>
2023-03-30 23:28:46 +0300

    Update ohp.sh

--- ohp/ohp.sh
+++ ohp/ohp.sh
@@ -15,7 +15,7 @@ LIGHT='\033[0;37m'
 # Getting
 MYIP=$(wget -qO- ipinfo.io/ip);
 echo "Checking VPS"
-IZIN=$( curl https://raw.githubusercontent.com/Slehibot/SLv2ray/main/ipvps.txt | grep $MYIP )
+IZIN=$( curl https://raw.githubusercontent.com/Rolka111111/SLv2ray/main/ipvps.txt | grep $MYIP )
 if [ $MYIP = $MYIP ]; then
 echo -e "${NC}${GREEN}Permission Accepted...${NC}"
 else
