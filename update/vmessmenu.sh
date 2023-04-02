ac143be32461440d425a3185abc06e92492d960d
Rolka111111 <90156401+Rolka111111@users.noreply.github.com>
2023-04-02 21:42:19 +0300

    Update vmessmenu.sh

--- update/vmessmenu.sh
+++ update/vmessmenu.sh
@@ -5,9 +5,9 @@ y="\033[0;1;37m"
 yy="\033[0;1;32m"
 yl="\033[0;1;33m"
 wh="\033[0m"
-echo -e "$y-------------------------------------------------$wh"
-echo -e "$y                     VMESS $wh"
-echo -e "$y-------------------------------------------------$wh"
+echo -e "$y-------------------------------------------------$wh" |lolcat
+echo -e "$y                     VMESS $wh" |lolcat
+echo -e "$y-------------------------------------------------$wh" |lolcat
 echo -e "$yy 1$y. Create Account XRAYS Vmess Websocket"
 echo -e "$yy 2$y. Delete Account XRAYS Vmess Websocket"
 echo -e "$yy 3$y. Extending Account XRAYS Vmess Active Life"
@@ -15,7 +15,7 @@ echo -e "$yy 4$y. Check User Login XRAYS Vmess"
 echo -e "$yy 5$y. Renew Certificate XRAYS Account"
 echo -e "$yy 6$y. Main Menu"
 echo -e "$yy 7$y. Exit"
-echo -e "$y-------------------------------------------------$wh"
+echo -e "$y-------------------------------------------------$wh" |lolcat
 read -p "Select From Options [ 1 - 7 ] : " menu
 echo -e ""
 case $menu in
